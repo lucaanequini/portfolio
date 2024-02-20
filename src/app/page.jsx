0a494ab090fc8e75783353370958b7f4dea38b71
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-02-20 13:08:24 -0300

    fix header Links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,23 +10,25 @@ export default function Home() {
   return (
     <>
       <div className='flex flex-col items-center md:px-5 xl:px-36'>
-        <div className='flex flex-col gap-5 md:items-center lg:flex-row lg:mt-20 lg:w-screen xl:px-32'>
+        <div id='inicio' className='flex flex-col gap-5 md:items-center lg:flex-row lg:mt-20 lg:w-screen xl:px-32'>
           <HelloContent></HelloContent>
           <Image src={'/real.png'} width={450} height={450} quality={100} alt='profilePic' priority className='rounded-full'></Image>
         </div>
+        <div id='about-me'></div>
         <div className="flex flex-col w-screen items-center justify-between gap-20 mt-40">
           <CardAb></CardAb>
           <div className='flex flex-col gap-5 text-center'>
             <Image src={'/gif2.gif'} width={350} height={350} quality={100} alt='gif' priority className='drop-shadow-2xl'></Image>
           </div>
         </div>
+        <div id='projects'></div>
         <div className='flex flex-col items-center justify-center mt-40'>
           <CardArea></CardArea>
         </div>
         <div className='mt-40'>
           <CertificatesSlide></CertificatesSlide>
         </div>
-        <div className='mt-40 mb-10'>
+        <div id="contact" className='mt-40 mb-10'>
           <ContactSection></ContactSection>
         </div>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,34 +7,38 @@ import Image from 'next/image';
 export default function Header() {
     const [isOpen, setIsOpen] = useState(false);
 
+    const handleCloseMenu = () => {
+        setIsOpen(false);
+    };
+
     return (
         <header className='flex justify-between text-center w-screen text-xl font-bold sticky top-0 z-50 left-0 backdrop-blur-lg md:px-5 xl:px-36'>
             <div className="flex items-center gap-5 ml-2">
-                <Image src={'/vetor.png'} alt={'vetor'} width={70} height={70} />
+                <Link href='#inicio'><Image src={'/vetor.png'} alt={'vetor'} width={70} height={70} /></Link>
             </div>
-            <nav className='hidden md:flex items-center gap-5 text-xl font-bold bg-gradient-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent'>
-                <Link href='/'>
+            <nav className='hidden lg:flex items-center gap-5 text-xl font-bold bg-gradient-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent'>
+                <Link href='#about-me' onClick={handleCloseMenu}>
                     About Me
                 </Link>
-                <Link href='/'>
+                <Link href='#projects' onClick={handleCloseMenu}>
                     Projects
                 </Link>
-                <Link href='/'>
+                <Link href='#contact' onClick={handleCloseMenu}>
                     Contact
                 </Link>
             </nav>
-            <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+            <button className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
                 <i className="bi bi-list icons text-3xl bg-gradient-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent"></i>
             </button>
             {isOpen && (
-                <div className="flex flex-col gap-3 md:hidden absolute right-0 top-full bg-gray-100 w-full">
-                    <Link href='/'>
+                <div className="flex flex-col gap-3 lg:hidden absolute right-0 top-full bg-custom-gray text-white w-full backdrop-blur-lg">
+                    <Link href='#about-me' onClick={handleCloseMenu}>
                         About Me
                     </Link>
-                    <Link href='/'>
+                    <Link href='#projects' onClick={handleCloseMenu}>
                         Projects
                     </Link>
-                    <Link href='/'>
+                    <Link href='#contact' onClick={handleCloseMenu}>
                         Contact
                     </Link>
                 </div>
