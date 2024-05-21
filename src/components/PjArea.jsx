4b8e9e624e681670f81ab4607bc3b830ca9020ea
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-05-21 13:49:57 -0300

    style: change projects sequence

--- src/components/PjArea.jsx
+++ src/components/PjArea.jsx
@@ -7,46 +7,14 @@ export default function CardArea() {
     const projects = [
         {
             id: 1,
-            title: 'Spotify Clone',
-            src: '/spotify.png',
-            url: ['https://github.com/lucaanequini/react-spotify'],
-            description: "O Spotify Clone (Front-End) é uma recriação do front-end da página inicial do Spotify, desenvolvido originalmente durante a imersão front-end do Alura.",
-            btnContent: ['Ver no GitHub']
+            title: 'Jotion',
+            src: '/jotion.png',
+            url: ['https://github.com/lucaanequini/notion-clone', 'https://note-taking-app-woad-eight.vercel.app/'],
+            description: 'Clone do Notion, com todas as funcionalidades! Desenvolvido com TypeScript, Tailwind CSS, Convex e Clerk Authentication.',
+            btnContent: ['Ver no GitHub', 'Acessar']
         },
         {
             id: 2,
-            title: 'Gestor de Estoque',
-            src: '/stock-manager.png',
-            url: ['https://github.com/lucaanequini/stock-manager'],
-            description: 'Simulador de gestor de estoque desenvolido com ReactJS, dentre suas funcionalidades temos: Adicionar, Remover e Editar produto, vizualizar detalhes de um produto e vizualições perante a data de cadastro do produto.',
-            btnContent: ['Ver no GitHub']
-        },
-        {
-            id: 3,
-            title: 'OneBitX',
-            src: '/falcon.png',
-            url: ['https://github.com/lucaanequini/OneBitX'],
-            description: 'Clone da página da SpaceX desenvolvida com HTML e SASS.',
-            btnContent: ['Ver no GitHub']
-        },
-        {
-            id: 4,
-            title: 'Weather APP',
-            src: '/weather-app.png',
-            url: ['https://github.com/lucaanequini/weather-project'],
-            description: 'Aplicação Clima-Tempo desenvolvida com NextJS, ReactJS e Tailwind CSS, utilizando a API REST "openweathermap" e o armazenamento do navegador.Pesquisar o clima de certa cidade, favoritar e desfavoritar cidades, assim como atualizar os dados das cidades favoritadas são algumas das funcionalidades.',
-            btnContent: ['Ver no GitHub']
-        },
-        {
-            id: 5,
-            title: 'TO DO List',
-            src: '/to-do-list.png',
-            url: ['https://github.com/lucaanequini/to-do-list'],
-            description: 'Aplicação de Lista de Afazeres desenvolvida com EJS, JS e banco de dados NoSQL (MongoDB). Adicionar, remover e editar listas de tarefas, adicionar, remover e marcar tarefas como conluídas são algumas das funcionalidades.',
-            btnContent: ['Ver no GitHub']
-        },
-        {
-            id: 6,
             title: 'OneBitFlix',
             src: '/onebitflix.png',
             url: ['https://github.com/lucaanequini/onebitflix-frontend', 'https://github.com/lucaanequini/onebitflix-backend'],
@@ -54,12 +22,44 @@ export default function CardArea() {
             btnContent: ['Ver no GitHub (Front-End)', 'Ver no GitHub (Back-End)']
         },
         {
+            id: 3,
+            title: 'Spotify Clone',
+            src: '/spotify.png',
+            url: ['https://github.com/lucaanequini/react-spotify'],
+            description: "O Spotify Clone (Front-End) é uma recriação do front-end da página inicial do Spotify, desenvolvido originalmente durante a imersão front-end do Alura.",
+            btnContent: ['Ver no GitHub']
+        },
+        {
+            id: 4,
+            title: 'Gestor de Estoque',
+            src: '/stock-manager.png',
+            url: ['https://github.com/lucaanequini/stock-manager'],
+            description: 'Simulador de gestor de estoque desenvolido com ReactJS, dentre suas funcionalidades temos: Adicionar, Remover e Editar produto, vizualizar detalhes de um produto e vizualições perante a data de cadastro do produto.',
+            btnContent: ['Ver no GitHub']
+        },
+        {
+            id: 5,
+            title: 'OneBitX',
+            src: '/falcon.png',
+            url: ['https://github.com/lucaanequini/OneBitX'],
+            description: 'Clone da página da SpaceX desenvolvida com HTML e SASS.',
+            btnContent: ['Ver no GitHub']
+        },
+        {
+            id: 6,
+            title: 'Weather APP',
+            src: '/weather-app.png',
+            url: ['https://github.com/lucaanequini/weather-project'],
+            description: 'Aplicação Clima-Tempo desenvolvida com NextJS, ReactJS e Tailwind CSS, utilizando a API REST "openweathermap" e o armazenamento do navegador.Pesquisar o clima de certa cidade, favoritar e desfavoritar cidades, assim como atualizar os dados das cidades favoritadas são algumas das funcionalidades.',
+            btnContent: ['Ver no GitHub']
+        },
+        {
             id: 7,
-            title: 'Jotion',
-            src: '/jotion.png',
-            url: ['https://github.com/lucaanequini/notion-clone', 'https://note-taking-app-woad-eight.vercel.app/'],
-            description: 'Clone do Notion, com todas as funcionalidades! Desenvolvido com TypeScript, Tailwind CSS, Convex e Clerk Authentication.',
-            btnContent: ['Ver no GitHub', 'Acessar']
+            title: 'TO DO List',
+            src: '/to-do-list.png',
+            url: ['https://github.com/lucaanequini/to-do-list'],
+            description: 'Aplicação de Lista de Afazeres desenvolvida com EJS, JS e banco de dados NoSQL (MongoDB). Adicionar, remover e editar listas de tarefas, adicionar, remover e marcar tarefas como conluídas são algumas das funcionalidades.',
+            btnContent: ['Ver no GitHub']
         }
     ]
 
